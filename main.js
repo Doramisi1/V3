2b19e10c6d4ef9c56d31a216dd021f316b64d6ad
Doramisi1 <105469411+Doramisi1@users.noreply.github.com>
2022-09-14 21:27:17 +0700

    Update main.js

--- main.js
+++ main.js
@@ -42,6 +42,7 @@ global.db = new Low(
 global.DATABASE = global.db // Backwards Compatibility
 
 global.conn = new WAConnection()
+conn.version = [2, 2230, 15]
 let authFile = `${opts._[0] || 'RadBotZ'}.json`
 if (fs.existsSync(authFile)) conn.loadAuthInfo(authFile)
 if (opts['trace']) conn.logger.level = 'trace'
